ad9da15bb0b3982f9dd0866e18663ea28ac00496
Aiver Keith De Guzman <aiverkeith@gmail.com>
2021-09-14 01:59:19 +0800

    6
    TODO: Exercise

--- populate_rango.py
+++ populate_rango.py
@@ -13,38 +13,46 @@ def populate():
     python_pages = [
         {
             'title': 'Official Python Tutorial',
-            'url': 'http://docs.python.org/3/tutorial/'
+            'url': 'http://docs.python.org/3/tutorial/',
+            'views': 1
         },
         {
             'title': 'How to Think like a Computer Scientist',
-            'url': 'http://www.greenteapress.com/thinkpython/'
+            'url': 'http://www.greenteapress.com/thinkpython/',
+            'views': 2
         },
         {
             'title': 'Learn Python in 10 Minutes',
-            'url': 'http://www.korokithakis.net/tutorials/python/'
+            'url': 'http://www.korokithakis.net/tutorials/python/',
+            'views': 3
         }
     ]
 
     django_pages = [
         {
             'title': 'Official Django Tutorial',
-            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'
+            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views': 4
         },
         {
             'title': 'Django Rocks',
-            'url': 'http://www.djangorocks.com/'
+            'url': 'http://www.djangorocks.com/',
+            'views': 5
         },
         {
             'title': 'How to Tango with Django',
-            'url': 'http://www.tangowithdjango.com/'
+            'url': 'http://www.tangowithdjango.com/',
+            'views': 6
         }
     ]
 
     other_pages = [
         {'title': 'Bottle',
-         'url': 'http://bottlepy.org/docs/dev/'},
+         'url': 'http://bottlepy.org/docs/dev/',
+            'views': 7},
         {'title': 'Flask',
-         'url': 'http://flask.pocoo.org'}]
+         'url': 'http://flask.pocoo.org',
+            'views': 8}]
 
     cats = {'Python': {'pages': python_pages,
                        'views': 128,
@@ -59,7 +67,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -70,6 +78,7 @@ def add_page(cat, title, url, views=0, likes=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
+    p.likes = likes
     p.save()
     return p
 
--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,26 @@
+from django.db import close_old_connections
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {
-        "bold_message": "Cupcake",
+        'bold_message': 'Cruncy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
     }
 
-    return render(request, 'rango/index.html', context=context_dict)
+    try:
+        top_five_pages = Page.objects.order_by('-views')[:5]
+        print('top_five_pages', top_five_pages)
+        context_dict['top_five_pages'] = top_five_pages
+    except Page.DoesNotExist:
+        context_dict['top_five_pages'] = None
+
+    return render(request, 'rango/index.html', context_dict)
     # return HttpResponse("Rango says hey")
 
 
@@ -22,3 +34,18 @@ def about(request):
         }
     )
     # return HttpResponse("Rango says here is the about page")
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context_dict)

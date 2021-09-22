a2a8c197767ad8824b70c1534b934ff2eda4bb28
Aiver Keith De Guzman <aiverkeith@gmail.com>
2021-09-23 00:11:29 +0800

    Setup the secret key lol

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,7 +23,12 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!&u!t7()bf-$xl8xmsv7s14@*0y1%9jtjvm6(1r=kh1s)tpv3n'
+
+key = None
+with open('/home/aiverkeith/secret.key') as f:
+    key = f.read().strip()
+
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
